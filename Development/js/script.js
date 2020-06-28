169630d39e6abc0b21b1d6cbd20cc3da7ddb8250
FatimaSaif935 <66377719+FatimaSaif935@users.noreply.github.com>
2020-06-28 14:41:55 +0300

    تم

---
+++ Development/js/script.js
@@ -0,0 +1,41 @@
+// السؤال الأول
+// 1
+let name = (" فاطمة دخيل");
+// 2
+let age = 18;
+// 3 and 4
+console.log ("أنا "+ name +" " + "و عمري " + age );
+// 5 and 6
+console.log (age*5);
+// -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
+
+// السؤال الثاني
+// 1
+let hobbies = ["القراءة", "الكتابة","الرسم"];
+// 2
+console.log(hobbies);
+console.push(age);
+console.log(hobbies+ " " +age);
+// 3
+console.log(hobbies);
+console.pop(hobbies);
+// 4
+let info = { masar = "web",
+favorite = "css",
+};
+// 5
+let ta = ["نانسي", "رهف", "حسين", "موضي"];
+console.log(info+" "+ta);
+// 6
+console.log(info.masar);
+// 7
+console.log(ta[0]);
+// -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
+
+// البونص
+// 1
+info ={ sentence = "مرحبا بكم",
+};
+// 2
+console.log(info.sentence);
+// console results => console.png
